18b14546c956c71be27f489a33e5423dc1cf2148
Andhika Surya <78678469+andhikarsurya@users.noreply.github.com>
2023-04-05 21:07:22 +0700

    Add files via upload

---
+++ app/scripts/applangs.js
@@ -0,0 +1,22 @@
+﻿/*!
+ This file is part of DecSoft App Builder.
+ Visit our website for license information.
+ Copyright ©2022 DecSoft Utils. All rights reserved.
+ Visit our website at: https://www.decsoftutils.com
+ */
+
+window.appLangs = [];
+
+window.appLangs["en"] = {
+
+  language: {
+    name: "English"
+  },
+
+  resources: {
+  },
+  sidebar: {
+    items: []
+  }
+};
+// End of window.appLangs["en"] = {
